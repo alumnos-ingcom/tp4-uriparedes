f79fe73060a6eb903efbfea499ee253c394ab1f5
uriparedes <urielchinoparedes@gmail.com>
2021-05-22 19:33:43 -0300

    Los ejercicios no estaban bien numerados dentro del codigo, intento de corregir un error en ejercicio 1 (suma restringida)

---
+++ Ejercicios/tp4ej8.py
@@ -0,0 +1,20 @@
+################
+# Maximiliano Uriel Paredes - @uriparedes
+# Ejercicio 7
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def ordenar_mayor_a_menor(uno, dos, tres):
+    temporal = uno
+    orden = [uno, dos, tres]
+    for x in orden:
+        
+    return orden
+def ordenar_menor_a_mayor(uno, dos, tres):
+    pass
+
+def prueba():
+    ordenar_mayor_a_menor(uno, dos, tres)
+
+if __name__ == "__main__":
+    prueba()
