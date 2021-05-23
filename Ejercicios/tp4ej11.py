9b767bcba4074419727f63020a27951516ea0796
uriparedes <urielchinoparedes@gmail.com>
2021-05-23 19:53:17 -0300

    Pequeñas correcciones al ejercicio 11 (palindromo)

--- Ejercicios/tp4ej11.py
+++ Ejercicios/tp4ej11.py
@@ -7,16 +7,16 @@
 
 def es_palindromo(texto):
     """toma un string, calcula la longitud y compara las letras en los extremos. Devuelve true si es un palindromo y false si no lo es"""
-    for i in range(int(len(texto)/2)):
+    for i in range(len(texto)//2):
         j = len(texto) -1 - i
         if(texto[i] != texto[j]):
             return False
     return True
     
 def prueba():
-    palabra = "asdfghjklññlkjhgfdsa"
+    palabra = input("ingrese una palabra: ")
     if(es_palindromo(palabra)):
-        print(f"La palabra '{palabra}' es un palindrom!")
+        print(f"La palabra '{palabra}' es un palindromo!")
     else:
         print(f"La palabra '{palabra}' no es un palindroma!")
 
