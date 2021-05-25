9a8090b0ce1ac951957304961c48be0d55f1c280
uriparedes <urielchinoparedes@gmail.com>
2021-05-25 16:40:43 -0300

    Agregado de manera recursiva la resolucion del ejercicio 8

--- Ejercicios/tp4ej8.py
+++ Ejercicios/tp4ej8.py
@@ -5,14 +5,14 @@
 ################
 # import random
 
-# def ordenar_mayor_a_menor(lista):#lo cambie a que reciba directamente un array para que sea mas facil la recursividad
-#     """Recibe un array y devuelve el mismo pero ordenado de menor a mayor"""
-#     for x in range(len(lista)-1):#llega hasta la longitud -1 porque cuando compara, compara al numero actual contra el siguiente
-#         if (lista[x] < lista[x+1]):#compara el valor actual con el siguiente
-#             lista[x], lista[x+1] = lista[x+1], lista[x] #intercambia los valores
-#             lista = ordenar_mayor_a_menor(lista)#vuelve a llamar la funcion para corregir los elementos
-#     return lista
-# 
+def ordenar_mayor_a_menor_recursivo(lista):#lo cambie a que reciba directamente un array para que sea mas facil la recursividad
+    """Recibe un array y devuelve el mismo pero ordenado de menor a mayor"""
+    for x in range(len(lista)-1):#llega hasta la longitud -1 porque cuando compara, compara al numero actual contra el siguiente
+        if (lista[x] < lista[x+1]):#compara el valor actual con el siguiente
+            lista[x], lista[x+1] = lista[x+1], lista[x] #intercambia los valores
+            lista = ordenar_mayor_a_menor(lista)#vuelve a llamar la funcion para corregir los elementos
+    return lista
+#
 # def ordenar_menor_a_mayor(lista):
 #     """Recibe un array y devuelve el mismo pero ordenado de menor a mayor"""
 #     for x in range(len(lista)-1):#llega hasta la longitud -1 porque cuando compara, compara al numero actual contra el siguiente
@@ -20,7 +20,7 @@
 #             lista[x], lista[x+1] = lista[x+1], lista[x] #intercambia los valores
 #             lista = ordenar_menor_a_mayor(lista)#vuelve a llamar la funcion para corregir los elementos
 #     return lista
-# 
+#
 # def prueba():
 #     desordenada = list(range(10))#crea una lista de n cantidad de numeros
 #     random.shuffle(desordenada)#ordena de manera aleatoria los numeros de la lista
@@ -55,6 +55,6 @@ def prueba():
     print(f"Tupla ordenada de mayor a menor: {tupla}")
     tupla = ordenar_menor_a_mayor(1, 3, 2)
     print(f"Tupla ordenada de menor a mayor: {tupla}")
-    
+
 if __name__ == "__main__":
     prueba()
